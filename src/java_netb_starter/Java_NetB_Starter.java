eed6ab74c656478cec03daeed5fdaacfddfdcd33
gdstitt <greg.stitt@bigpond.com>
2017-12-30 18:22:57 +0800

    1st Commit - initial file

---
+++ src/java_netb_starter/Java_NetB_Starter.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Gregory Stitt
+ * 
+ */
+package java_netb_starter;
+
+import java.lang.annotation.Documented;
+
+/**
+ * Starting point for a Java application with JUnit tests
+ * @author Greg
+ */
+public class Java_NetB_Starter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
