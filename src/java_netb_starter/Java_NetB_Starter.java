26a64f79421c94348767a736cd11002b9e8d6b80
gdstitt <greg.stitt@bigpond.com>
2017-12-30 19:48:20 +0800

    2nd Commit - added @Document @interface ClassPreamble

--- src/java_netb_starter/Java_NetB_Starter.java
+++ src/java_netb_starter/Java_NetB_Starter.java
@@ -6,10 +6,28 @@ package java_netb_starter;
 
 import java.lang.annotation.Documented;
 
+@Documented
+@interface ClassPreamble {
+
+    String author() default "Greg Stitt";
+
+    String date() default "N/A";
+
+    int version() default 1;
+
+    String lastModifiedBy() default "N/A";
+
+    String lastModified() default "N/A";
+
+    String[] reviewers() default "N/A";
+}
+
 /**
  * Starting point for a Java application with JUnit tests
+ *
  * @author Greg
  */
+@ClassPreamble()
 public class Java_NetB_Starter {
 
     /**
@@ -18,5 +36,5 @@ public class Java_NetB_Starter {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
